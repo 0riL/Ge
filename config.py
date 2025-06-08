37d99968fd5d6345bc15361b3eef97128be59c6f
0riL <ipfyekly@gmail.com>
2025-06-08 22:27:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
 import os
 
-GITHUB_PAT = os.getenv("GITHUB_PAT")
-GITHUB_REPO = os.getenv("GITHUB_REPO")  # format: username/repo
-GITHUB_BRANCH = os.getenv("GITHUB_BRANCH", "main")
-BOT_SPEED = int(os.getenv("BOT_SPEED", "1"))  # usernames per second
+BOT_SPEED = int(os.getenv("BOT_SPEED", "1"))  # Set this in Render environment
+LOG_FILE = "checked_usernames.txt"
