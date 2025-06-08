c76f9b021c9eaebec76363b088152535d5f416dd
0riL <ipfyekly@gmail.com>
2025-06-08 22:37:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,4 +8,5 @@ def index():
     return send_from_directory('static', 'index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
